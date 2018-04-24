236960445183ab8f2777f0371e53f804d7660f87
loryu <chinaloryu@gmail.com>
2018-04-24 20:16:31 +0800

    Delete reboot9003.sh

--- reboot9003.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export TOMCAT_OSPID=`netstat -antpu|grep 9003|grep java|awk '{print $7}'`
-
-/bin/kill -9 ${TOMCAT_OSPID%/*}
-
-/bin/sh /letv/services/9003/bin/startup.sh 
-# -Djava.awt.headless=true
